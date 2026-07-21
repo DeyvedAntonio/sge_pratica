70b218cd47d275bb3ba8f037d4eb357d2ae11520
Deyved Antonio <deyved.antonio@gmail.com>
2026-07-21 16:04:11 -0300

    feat: create model of brands.

--- brands/models.py
+++ brands/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from app.models import BaseModel
 
-# Create your models here.
+
+class Brand(BaseModel):
+
+    name = models.CharField(
+        'nome',
+        max_length=150,
+    )
+    description = models.TextField(
+        'descrição',
+        null=True,
+        blank=True,
+    )
+
+    class Meta:
+        ordering = ['name',]
+        verbose_name = 'Marca'
+        verbose_name_plural = 'Marcas'
