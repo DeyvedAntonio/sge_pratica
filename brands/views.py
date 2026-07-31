b8cb6f78db9f187bb9c9f3023ed61d4fa90bd3aa
Deyved Antonio <deyved.antonio@gmail.com>
2026-07-31 11:13:45 +0000

    feat: brands' routes and create brand's template.

--- brands/views.py
+++ brands/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Brand
 
-# Create your views here.
+
+class BrandListView(ListView):
+    model = Brand
+    template_name = 'brand_list.html'
+    context_object_name = 'brands'
