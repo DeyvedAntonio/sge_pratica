29665088c320c9348817ea85c689fce62d3580c9
Deyved Antonio <deyved.antonio@gmail.com>
2026-08-26 16:32:26 -0300

    feat: category model's update.

---
+++ categories/urls.py
@@ -0,0 +1,38 @@
+from django.contrib.urls import path
+from .views import (
+    CategoryCreateView,
+    CategoryDeleteView,
+    CategoryDetailView,
+    CategoryListView,
+    CategoryUpdateView,
+)
+
+app_name = 'category'
+
+urlspatterns = [
+    path(
+        'list/',
+        CategoryListView.as_view(),
+        name='category-list',
+    ),
+    path(
+        'create/',
+        CategoryCreateView.as_view(),
+        name='category-create',
+    ),
+    path(
+        '<str: uuid>/delete/',
+        CategoryDeleteView.as_view(),
+        name='category-delete',
+    ),
+    path(
+        '<str: uuid>/detail/',
+        CategoryDetailView.as_view(),
+        name='category-detail',
+    ),
+    path(
+        '<str:uuid>/update/',
+        CategoryUpdateView.as_view(),
+        name='category-update',
+    ),
+]
--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    UpdateView,
+)
+from .models import Category
+from .forms import CategoryForm
 
-# Create your views here.
+
+class CategoryCreateView(CreateView):
+
+    model = Category
+    template_name = 'category_create.html'
+    form_class = CategoryForm
+    success_url = reverse_lazy('category-list')
+
+
+class CategoryDeleteView(DeleteView):
+
+    model = Category
+    template_name = 'category_delete.html'
+    success_url = reverse_lazy('category:category-list')
+    slug_field = 'uuid'
+    slug_url_kwarg = 'uuid'
+
+
+class CategoryDetailView(DetailView):
+
+    model = Category
+    template_name = 'category_detail.html'
+    context_object_name = 'categories'
+    slug_field = 'uuid'
+    slug_url_kwarg = 'uuid'
+
+
+class CategoryListView(ListView):
+
+    model = Category
+    template_name = 'category_list.html'
+    context_object_name = 'categories'
+
+
+class CategoryUpdateView(UpdateView):
+
+    model = Category
+    template_name = 'category_update.html'
+    form_class = CategoryForm
+    success_url = reverse_lazy('category:category-list')
+    slug_field = 'uuid'
+    slug_url_kwarg = 'uuid'
