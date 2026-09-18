72f48ff7ab7418c6b8b220f9896074f8458f9172
Deyved Antonio <deyved.antonio@gmail.com>
2026-09-18 17:10:30 -0300

    feat: created page home.

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from .views import home
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', home, name='home'),
     path("brands/", include("brands.urls")),
     path('categories/', include('categories.urls')),
     path('supliers/', include('supliers.urls')),
